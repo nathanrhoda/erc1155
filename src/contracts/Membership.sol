4af4a3deae1f60869e595da1fbc47b4116981e08
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-05-14 13:53:47 +0200

    Approving and purchasing memberships

--- src/contracts/Membership.sol
+++ src/contracts/Membership.sol
@@ -14,12 +14,16 @@ contract Membership  is ERC1155 {
   }
 
   uint256 public TokenCounter;
+  address private owner;
 
   constructor() ERC1155("https://") {
     TokenCounter = 0;
+    owner = msg.sender;
   }
 
-  function publish(string memory membershipType, uint256 _count) public {
+  function publish(string memory membershipType, uint256 _count) 
+    public 
+  {
     uint256 newTokenId = TokenCounter+1;
     _membershipDetails[newTokenId].author = msg.sender;
     _membershipDetails[newTokenId].membershipType = membershipType;
@@ -27,4 +31,38 @@ contract Membership  is ERC1155 {
     _mint(msg.sender, newTokenId,_count, "");
     TokenCounter += 1;
   } 
+
+  function Approve(address operator)
+    public 
+  {
+    require(operator != msg.sender);
+    setApprovalForAll(operator, true);
+  }
+
+  function purchaseFromAuthor(uint256 TokenId, uint256 _numOfMembership)
+    public
+  {
+    safeTransferFrom(_membershipDetails[TokenId].author, msg.sender, TokenId, _numOfMembership, "");
+  }  
+
+  function TypeOfMembership(uint256 membershipId) 
+    public view 
+    returns (string memory)   
+  {
+    return (_membershipDetails[membershipId].membershipType);
+  }
+
+  function TotalCountOfMembers(uint256 membershipId) 
+    public view 
+    returns(uint256) 
+  {
+    return (_membershipDetails[membershipId].membershipCount);
+  }
+
+  function AuthorOfMembership(uint membershipId)
+    public view
+    returns (address)
+  {
+    return (_membershipDetails[membershipId].author);
+  }
 }
